7c98dc117243c0faec14fef8173715eb461b4c55
Renan <renan@grama.cc>
2018-06-26 16:39:07 -0300

    cors

--- main.go
+++ main.go
@@ -28,11 +28,14 @@ func main() {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	
 	fmt.Fprintf(w, "Nada aqui, mas você pode acessar /login e /auth, tem coisa lá =)")
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(200)
 
 	type Response struct {
